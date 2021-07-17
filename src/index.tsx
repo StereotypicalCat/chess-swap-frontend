36a6023d301a96011b3d195aa56e0be133f57354
Lucas Winther <lucasw89@live.dk>
2021-07-17 22:57:18 +0200

    Got Chess Board back into the app

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ChessSwap from "./ChessSwap";
+
+// Chat Example
+/*ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChessSwap />
   </React.StrictMode>,
   document.getElementById('root')
 );
